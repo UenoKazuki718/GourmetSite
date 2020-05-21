dd89137ffcbead18d29bf7658a68d3fb81069d6c
上野一樹 <uenokazuki@uenokazukinoMacBook-Pro.local>
2020-05-21 10:32:53 +0900

    ‘21日朝修正版’

---
+++ グル渋/src/グル渋/Book.java
@@ -0,0 +1,49 @@
+package グル渋;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Book
+ */
+@WebServlet("/Book")
+public class Book extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Book() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String date = request.getParameter("date");
+		int num = Integer.parseInt(request.getParameter("num"));
+		String course = request.getParameter("course");
+		System.out.print(date);
+		//サーバーに保存
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/detail.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
